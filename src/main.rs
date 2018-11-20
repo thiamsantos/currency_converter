4aaf87f05e6fb199453e74a0279045e3cf9d61c7
Thiago Santos <thia.mdossantos@gmail.com>
2018-11-20 21:56:40 -0200

    refactor: break functions

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,68 @@ extern crate reqwest;
 #[macro_use]
 extern crate serde_derive;
 
-use reqwest::Response;
 use std::collections::HashMap;
 use std::env;
+
+struct Arguments<'a> {
+    base: &'a str,
+    symbol: &'a str,
+    value: f64
+}
 
 #[derive(Deserialize)]
 struct CurrencyRatesResponse {
     rates: HashMap<String, f64>,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 4 {
-        panic!("You must provide three arguments: base, symbol and value");
-    }
-
-    let base = &args[1];
-    let symbol = &args[2];
-    let value = args[3].parse::<f64>().unwrap();
-
-    let client = reqwest::Client::new();
-    let body = client
-        .get("https://api.exchangeratesapi.io/latest")
-        .query(&[("base", base), ("symbols", symbol)])
-        .send();
-
-    match body {
-        Ok(response) => handle_response(response, base, symbol, value),
-        Err(err) => println!("error = {:?}", err),
-    }
+struct CurrencyRate<'a> {
+    base: &'a str,
+    symbol: &'a str,
+    rate: f64
 }
 
-fn handle_response(mut response: Response, base: &str, symbol: &str, value: f64) {
-    let rates: CurrencyRatesResponse = response.json().unwrap();
-    let rate = rates.rates[symbol];
-    let total = rate * value;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let parsed_arguments = parse_arguments(&args);
+    let currency_rate = fetch_rate(&parsed_arguments);
+    let currency_value = calculate_currency_value(parsed_arguments.value, currency_rate.rate);
+    show_report(&parsed_arguments, &currency_rate, currency_value);
+}
 
-    println!("{:.2} {} worths {:.2} {}", value, base, total, symbol)
+fn parse_arguments(args: &Vec<String>) -> Arguments {
+    let arguments = Arguments {
+        base: &args[1],
+        symbol: &args[2],
+        value: args[3].parse::<f64>().unwrap()
+    };
+
+    arguments
+}
+
+
+fn fetch_rate<'a>(arguments: &'a Arguments) -> CurrencyRate<'a> {
+    let client = reqwest::Client::new();
+    let mut response = client
+        .get("https://api.exchangeratesapi.io/latest")
+        .query(&[("base", arguments.base), ("symbols", arguments.symbol)])
+        .send()
+        .expect("Error");
+
+    let rates: CurrencyRatesResponse = response.json().unwrap();
+
+    let currency_rate = CurrencyRate {
+        base: arguments.base,
+        symbol: arguments.symbol,
+        rate: rates.rates[arguments.symbol]
+    };
+
+    currency_rate
+}
+
+fn calculate_currency_value(value: f64, rate: f64) -> f64 {
+    value * rate
+}
+
+fn show_report(arguments: &Arguments, currency_rate: &CurrencyRate, total: f64) {
+    println!("{:.2} {} worths {:.2} {}", arguments.value, currency_rate.base, total, currency_rate.symbol)
 }
